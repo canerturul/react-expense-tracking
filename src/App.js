d95992b4955d06ad9100a5eb2d28f9b0b8adeaa5
canerturul <canerturul@gmail.com>
2021-06-08 00:08:29 +0300

    readme added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
  
   const DUMMY_EXPENSES = [
-    {id:"e1", title:"Car Insurance", amount:265.54, date: new Date(2021, 2, 28)},
-    {id:"e2", title:"Car Insuranc", amount:264.54, date: new Date(2021, 2, 27)},
-    {id:"e3", title:"Car Insuran", amount:263.54, date: new Date(2021, 2, 26)},
-    {id:"e4", title:"Car Insura", amount:262.54, date: new Date(2021, 2, 25)},
+      {
+        id: 'e1',
+        title: 'Education',
+        amount: 94.12,
+        date: new Date(2021, 7, 14),
+      },
+      { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+      {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 1, 28),
+      },
+      {
+        id: 'e4',
+        title: 'New Table',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+      },{
+        id: 'e5',
+        title: 'Shoes',
+        amount: 50,
+        date: new Date(2020, 5, 12),
+      },
   ];
   
   const[expenses,setExpenses ] = useState(DUMMY_EXPENSES)
